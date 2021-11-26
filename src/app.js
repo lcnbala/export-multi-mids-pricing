dbea14d8b5ea998c05a629ef8267b934ee40ce8c
Lucian Bala <2097537+undefined@users.noreply.github.com>
2021-11-26 11:03:38 +0000

    First commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+  title = "Welcome to Lightning Web Components!";
+
+  showFeatures = true;
+
+  /**
+   * Getter for the features property
+   */
+  get features() {
+    return [
+      {
+        label: "Learn in the browser.",
+        icon: "utility:edit",
+      },
+      {
+        label: "View changes to code instantly with Live Compilation.",
+        icon: "utility:refresh",
+      },
+      {
+        label: "Style your components with SLDS.",
+        icon: "utility:brush",
+      },
+    ];
+  }
+}
